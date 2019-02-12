cdb92334fac9f5968f922194223aab1b41a9aa78
Mohit <mohit.soni@ranosys.com>
2019-02-12 12:04:43 +0530

    another try

--- capthook/views.py
+++ capthook/views.py
@@ -88,17 +88,24 @@ def handle_webhook(event, payload):
 def handle_github_hook(request):
     # Check the X-Hub-Signature header to make sure this is a valid request.
     # github_signature = request.META['HTTP_X_HUB_SIGNATURE']
+    print(request.META)
+    print(request.META.get('HTTP_X_HUB_SIGNATURE'))
     github_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
     signature = hmac.new(settings.GITHUB_WEBHOOK_SECRET, request.body, hashlib.sha1)
     expected_signature = 'sha1=' + signature.hexdigest()
-    if not hmac.compare_digest(github_signature, expected_signature):
-        return HttpResponseForbidden('Invalid signature header')
+    # if not hmac.compare_digest(github_signature, expected_signature):
+    #     return HttpResponseForbidden('Invalid signature header')
 
     # Sometimes the payload comes in as the request body, sometimes it comes in
     # as a POST parameter. This will handle either case.
     if 'payload' in request.POST:
         payload = json.loads(request.POST['payload'])
     else:
+        print("*******************************")
+        print(request.body)
+        print('------------------------')
+        print(request.POST)
+        print("*******************************")
         payload = json.loads(request.body)
 
     event = request.META['HTTP_X_GITHUB_EVENT']
